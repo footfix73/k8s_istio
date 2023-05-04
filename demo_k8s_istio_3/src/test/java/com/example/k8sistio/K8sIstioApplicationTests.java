5acf831f13af73cf26aa3c0099a41ba583fa8775
vicente.garcia <footfix73@gmail.com>
2023-05-04 13:40:43 +0200

    first update Microservice3

---
+++ demo_k8s_istio_3/src/test/java/com/example/k8sistio/K8sIstioApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.k8sistio;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class K8sIstioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
