1ff427b478615080697d2576c3112886e2c9e0e2
vicente.garcia <footfix73@gmail.com>
2023-05-09 13:10:41 +0200

    Ultima actualizacion

--- demo_k8s_istio_2/src/main/java/com/example/k8sistio/controller/Demo2Controller.java
+++ demo_k8s_istio_2/src/main/java/com/example/k8sistio/controller/Demo2Controller.java
@@ -1,10 +1,9 @@
 package com.example.k8sistio.controller;
 
-import java.time.LocalDate;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class Demo2Controller {
 
-	public static final String HTTP_DEMO_1_APP_8080_SERVICE_3 = "http://localhost:8083/service-3";
+	@Value("${micro.service3}")
+	private String service3;
+
 	private static final Log log = LogFactory.getLog(Demo2Controller.class);
 
 	@Autowired
@@ -28,7 +29,7 @@ public class Demo2Controller {
 	@GetMapping(value = "/service-2-3", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getValueFromService2() {
 		log.info("Llamando al servicio 2-3");
-		String val = restTemplate.getForObject(HTTP_DEMO_1_APP_8080_SERVICE_3, String.class);
+		String val = restTemplate.getForObject(service3, String.class);
 		return "Ha llamado al servicio 2-3 que ha llamadoa al servicio 3---" + val;
 	}
 
